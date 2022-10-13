1489a58700927aee6708a7cd5e7589763d9497b9
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2022-10-13 08:31:20 +0200

    used FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -20,57 +20,105 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List todos = [];
-  String url =
-      'https://jsonplaceholder.typicode.com/todos?completed=true&userId=1';
+  String url = 'https://jsonplaceholder.typicode.com/posts';
 
-  Future getTodos() async {
+  Future getPosts() async {
     var response = await http.get(Uri.parse(url));
 
     var responseBody = jsonDecode(response.body);
 
-    setState(() {
-      todos.addAll(responseBody);
-    });
-  }
-
-  @override
-  void initState() {
-    getTodos();
+    return responseBody;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todos API'),
-        centerTitle: true,
-      ),
-      body: todos == null || todos.isEmpty
-          ? const Center(
+        appBar: AppBar(
+          title: const Text('Posts API'),
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+          future: getPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.separated(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: ((context, index) => InkWell(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                if (snapshot.hasData) {
+                                  return SimpleDialog(
+                                    title: RichText(
+                                      text: TextSpan(
+                                          text:
+                                              'Post ${snapshot.data[index]['id']} ',
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black),
+                                          children: [
+                                            TextSpan(
+                                                text:
+                                                    'by user ${snapshot.data[index]['userId']}',
+                                                style: const TextStyle(
+                                                    fontStyle:
+                                                        FontStyle.italic))
+                                          ]),
+                                    ),
+                                    contentPadding: const EdgeInsets.all(20),
+                                    children: [
+                                      Text(
+                                        '${snapshot.data[index]['body']}',
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text("Close"))
+                                    ],
+                                  );
+                                }
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(10)),
+                          margin: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: RichText(
+                              text: TextSpan(
+                                  text:
+                                      'user${snapshot.data[index]['userId']}:',
+                                  style: const TextStyle(
+                                      color: Colors.white70, fontSize: 23),
+                                  children: [
+                                    TextSpan(
+                                      text: ' ${snapshot.data[index]['title']}',
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    )
+                                  ]),
+                            ),
+                          ),
+                        ),
+                      )),
+                  separatorBuilder: ((context, index) => const SizedBox(
+                        height: 10,
+                      )),
+                  itemCount: snapshot.data.length);
+            }
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.separated(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: ((context, index) => Container(
-                    decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(10)),
-                    margin: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        '${todos[index]['title']}',
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 25),
-                      ),
-                    ),
-                  )),
-              separatorBuilder: ((context, index) => const SizedBox(
-                    height: 5,
-                  )),
-              itemCount: todos.length),
-    );
+            );
+          },
+        ));
   }
 }
